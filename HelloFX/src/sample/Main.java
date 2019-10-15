8d1f3ad6072645fbecf250b59296741f5f207a41
RyanCardin15 <56137314+RyanCardin15@users.noreply.github.com>
2019-10-15 14:28:36 -0500

    Updated FXML files

--- HelloFX/src/sample/Main.java
+++ HelloFX/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Wassup my homies");
+        primaryStage.setTitle(".TXT FILE STORAGE");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
