e1e83c5f5b77f8ec91708e8fa11e9a628ddb0cc1
Sumedh Saurabh <sumedh50@gmail.com>
2019-10-18 14:24:54 -0500

    Set up Accoount Fucntions

--- HelloFX/src/sample/Account.java
+++ HelloFX/src/sample/Account.java
@@ -1,33 +1,48 @@
 package sample;
 
 public class Account {
-        private String username;
-        private boolean isAdmin;
-        private Controller controller;
-        private String fName;
-        private String lName;
-        private String email;
-        private String password;
+    private String username;
+    private boolean isAdmin;
+    private Controller controller;
+    private String fName;
+    private String lName;
+    private String email;
+    private String password;
 
-        public Account(){
-            this.username = "";
-            this.isAdmin = true;
-            this.fName = "";
-            this.lName = "";
-            this.email = "";
-            this.password="";
-        }
-        public Account(String u, boolean a, String f, String l, String e, String p){
-            this.username = u;
-            this.isAdmin = a;
-            this.fName = f;
-            this.lName = l;
-            this.email = e;
-            this.password = p;
-        }
+    public Account(){
+        this.username = "";
+        this.isAdmin = true;
+        this.fName = "";
+        this.lName = "";
+        this.email = "";
+        this.password="";
+    }
+    public Account(String u, boolean a, String f, String l, String e, String p){
+        this.username = u;
+        this.isAdmin = a;
+        this.fName = f;
+        this.lName = l;
+        this.email = e;
+        this.password = p;
+    }
 
-        @Override
-        public String toString() {
-            return "Account [fName=" + fName + ", lName=" + lName + ", username=" + username + ", email=" + email + ", password=" + password + "]";
-        }
+    @Override
+    public String toString() {
+        return "Account [fName=" + fName + ", lName=" + lName + ", username=" + username + ", email=" + email + ", password=" + password + "]";
+    }
+
+    public void setFName(String value) {
+    }
+
+    public void setLName(String data) {
+    }
+
+    public void setEmail(String data) {
+    }
+
+    public void setPassword(String data) {
+    }
+
+    public void setUsername(String data) {
+    }
 }
